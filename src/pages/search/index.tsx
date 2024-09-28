89605ae5621b0c0015c51b283d36c66c119efe87
jmeno1011 <jwhltn8282@gmail.com>
2024-09-28 14:36:07 +0900

    edit prettie

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -15,30 +15,30 @@ export default function Page() {
   // 최대한 ssg로 next build 결과에 따라서..!
   const result = movies.filter(movies => movies.title.includes(q));
 
-  const fetchSearchResult = async()=>{
+  const fetchSearchResult = async () => {
     const data = await fetchMovies(q);
     setMovies(data);
   }
 
-  useEffect(()=>{
-    if(q){
+  useEffect(() => {
+    if (q) {
       fetchSearchResult();
     }
-  },[q])
-  
+  }, [q])
+
   return (
     <>
-    <GlobalHead
+      <GlobalHead
         title={`${q} 한입 씨네마 검색`}
         ogImage="/thumnail.png"
         ogTitle="한입 씨네마"
         description="한입 씨네마에 등록된 영화들을 만나보세요"
       />
-  <div className={style.container}>
-    {result.length > 0 ? result.map(movie =>
-      <MovieItem key={movie.id} {...movie} />
-    ) : <div>검색 결과가 없습니다.</div>}
-  </div>
+      <div className={style.container}>
+        {result.length > 0 ? result.map(movie =>
+          <MovieItem key={movie.id} {...movie} />
+        ) : <div>검색 결과가 없습니다.</div>}
+      </div>
     </>
   );
 }
